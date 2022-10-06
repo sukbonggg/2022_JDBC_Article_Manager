c16d139f9ba4d2f043fc3bd602f6452e637f8d21
sukbonggg <kiti982611@gmail.com>
2022-10-06 19:29:32 +0900

    게시물 작성시 DB에 INSERT

--- src/com/koreaIT/example/JAM/Main.java
+++ src/com/koreaIT/example/JAM/Main.java
@@ -1,12 +1,16 @@
 package com.koreaIT.example.JAM;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		
 		Scanner sc = new Scanner(System.in);
 		
 		List<Article> articles = new ArrayList<>();
@@ -27,6 +31,48 @@ public class Main {
 				
 				Article article =new Article(id,title,body);
 				articles.add(article);
+				
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/jdbc_article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW()";
+					sql += ", updateDate = NOW()";
+					sql += ", title = '"+ title +"'";
+					sql += ", `body` = '"+ body +"';";
+
+					pstmt = conn.prepareStatement(sql);
+
+					pstmt.executeUpdate();
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				
 				lastArticleId++;
 				
 //				System.out.println(article);
