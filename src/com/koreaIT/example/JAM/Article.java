84309b7c88932df0042027aeb6393d814d68cf4f
sukbonggg <kiti982611@gmail.com>
2022-10-06 20:12:30 +0900

    게시물 리스팅 데이터를 DB로부터 가져오기

--- src/com/koreaIT/example/JAM/Article.java
+++ src/com/koreaIT/example/JAM/Article.java
@@ -5,15 +5,25 @@ public class Article extends Object{
 	int id;
 	String title;
 	String body;
-
+	String regDate;
+	String updateDate;
 	public Article(int id, String title, String body) {
 		this.id=id;
 		this.title=title;
 		this.body=body;
 	}
 
+	public Article(int id, String regDate, String updateDate, String title, String body) {
+		this.id=id;
+		this.regDate =regDate;
+		this.updateDate =updateDate;
+		this.title=title;
+		this.body=body;
+	}
+
 	@Override
 	public String toString() {
-		return "Article [id=" + id + ", title=" + title + ", body=" + body + "]";
+		return "Article [id=" + id + ", title=" + title + ", body=" + body + ", regDate=" + regDate + ", updateDate="
+				+ updateDate + "]";
 	}
 }
