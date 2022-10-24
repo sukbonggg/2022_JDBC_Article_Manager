a102151805415307b5f110932db4060a8e615493
sukbonggg <kiti982611@gmail.com>
2022-10-24 21:43:58 +0900

    게시물 수정,삭제시 권한 체크

--- src/com/koreaIT/example/JAM/controller/ArticleController.java
+++ src/com/koreaIT/example/JAM/controller/ArticleController.java
@@ -28,7 +28,7 @@ public class ArticleController extends Controller {
 		String title = sc.nextLine();
 		System.out.printf("내용 : ");
 		String body = sc.nextLine();
-		
+
 		int memberId = Container.session.loginedMemberId;
 
 		int id = articleService.doWrite(memberId, title, body);
@@ -50,7 +50,8 @@ public class ArticleController extends Controller {
 		System.out.println("번호	|	제목	|	작성자	|	조회	|	작성일");
 
 		for (Article article : articles) {
-			System.out.printf("%d	|	%s	|	%s	|	%d	|	%s\n", article.id, article.title, article.writerName, article.hit, article.updateDate);
+			System.out.printf("%d	|	%s	|	%s	|	%d	|	%s\n", article.id, article.title, article.writerName,
+					article.hit, article.updateDate);
 		}
 	}
 
@@ -58,7 +59,7 @@ public class ArticleController extends Controller {
 		int id = Integer.parseInt(cmd.split(" ")[2]);
 
 		articleService.increaseHit(id);
-		
+
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
@@ -85,13 +86,17 @@ public class ArticleController extends Controller {
 
 		int id = Integer.parseInt(cmd.split(" ")[2]);
 
-		boolean isArticleExists = articleService.isArticleExists(id);
+		Article article = articleService.getArticle(id);
 
-		if (isArticleExists == false) {
+		if (article == null) {
 			System.out.printf("%d번 게시글은 존재하지 않습니다\n", id);
 			return;
 		}
-		
+
+		if (article.memberId != Container.session.loginedMemberId) {
+			System.out.println("헤당 게시글에 대한 권한이 없습니다");
+			return;
+		}
 
 		System.out.printf("== %d번 게시물 수정 ==\n", id);
 
@@ -114,10 +119,15 @@ public class ArticleController extends Controller {
 
 		int id = Integer.parseInt(cmd.split(" ")[2]);
 
-		boolean isArticleExists = articleService.isArticleExists(id);
+		Article article = articleService.getArticle(id);
 
-		if (isArticleExists == false) {
+		if (article == null) {
 			System.out.printf("%d번 게시글은 존재하지 않습니다\n", id);
+			return;
+		}
+		
+		if (article.memberId != Container.session.loginedMemberId) {
+			System.out.println("헤당 게시글에 대한 권한이 없습니다");
 			return;
 		}
 
